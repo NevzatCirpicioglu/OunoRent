b05651518abd344dd4dd25cdea42c0d6e6af4bdb
ozgurgkmn <ozgurgokmen@yesilgezegen.com>
2024-07-18 10:09:44 +0300

    Add : Mvc Project for admin panel

---
+++ OunoRentAdminPanel/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OunoRentAdminPanel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
