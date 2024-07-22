384da9a2480387b4c1c9d5c11749c8f64bcaec9c
NevzatCirpicioglu <nevzatcancirpicioglu@yesilgezegen.com>
2024-07-22 10:20:22 +0300

    Done : Create Category Api Request

--- OunoRentAdminPanel/Controllers/CategoryController.cs
+++ OunoRentAdminPanel/Controllers/CategoryController.cs
@@ -1,19 +1,52 @@
 using Microsoft.AspNetCore.Mvc;
+using OunoRentAdminPanel.Models.EntityModels.CategoryModel;
 
 namespace OunoRentAdminPanel.Controllers;
 
 [Route("category")]
 public class CategoryController : Controller
 {
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public CategoryController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
     [HttpGet()]
     public IActionResult Index()
     {
         return View();
     }
 
-    [HttpGet("add-category")]
+    [HttpGet("addCategory")]
     public IActionResult AddCategory()
     {
         return View();
+    }
+
+    [HttpPost("addCategory")]
+    public async Task<IActionResult> AddCategory(CreateCategoryModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var client = _httpClientFactory.CreateClient("ounoRentApi");
+
+            var response = await client.PostAsJsonAsync("category", model);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+          else
+            {
+                ModelState.AddModelError(string.Empty, "Kategori eklenirken bir hata oluştu.");
+
+                return View(model);
+            }
     }
 }
