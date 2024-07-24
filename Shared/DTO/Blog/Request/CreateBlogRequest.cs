51e467d1164b5cf5de6114e8449b03ce1277647b
ozgurgkmn <ozgurgokmen@yesilgezegen.com>
2024-07-24 09:00:14 +0300

    Added : Blog Dto

---
+++ Shared/DTO/Blog/Request/CreateBlogRequest.cs
@@ -0,0 +1,6 @@
+namespace Shared.DTO.Blog.Request;
+
+public class CreateBlogRequest
+{
+
+}
---
+++ Shared/DTO/Blog/Response/BlogResponse.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shared.DTO.Blog.Response;
+
+public class BlogResponse
+{
+    
+}
