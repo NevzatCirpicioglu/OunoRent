d5e2c8678815794d4d9715c501d16d500036c0e0
NevzatCirpicioglu <nevzatcancirpicioglu@yesilgezegen.com>
2024-07-25 17:28:50 +0300

    Fixed

--- Shared/DTO/Blog/Request/UpdateBlogRequest.cs
+++ Shared/DTO/Blog/Request/UpdateBlogRequest.cs
@@ -6,5 +6,7 @@ using System.Threading.Tasks;
 namespace Shared.DTO.Blog.Request;
 
 public sealed record UpdateBlogRequest(
-    Guid BlogId, Guid SubCategoryId, string Title, string LargeImgUrl, string SmallImgUrl,
-     string Tags, string Slug,int OrderNumber, DateTime Date, Boolean IsActive);
+    Guid BlogId, Guid SubCategoryId, string Title,
+    string LargeImgUrl, string SmallImgUrl,
+    string Tags, string Slug,int OrderNumber,
+     DateTime Date, Boolean IsActive);
